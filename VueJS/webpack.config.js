e29f034667e5a840b124d2be834a6775eb4d6389
Arkadiusz Stępniewski <arkadiusz.stepniewski@dfg.com>
2019-12-02 19:50:09 +0100

    Add webpack with basic configuration

---
+++ VueJS/webpack.config.js
@@ -0,0 +1,12 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: {
+        'js/index/app': './wwwroot/src/js/index/app.js',
+        'js/about/app': './wwwroot/src/js/about/app.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname + '/wwwroot/dist'
+    }
+};
