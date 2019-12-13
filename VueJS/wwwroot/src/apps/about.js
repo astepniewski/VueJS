9b6c4b9c07aea0c389b7eceb5fb1c811b966a3d5
Arkadiusz Stępniewski <arkadiusz.stepniewski@dfg.com>
2019-12-13 18:45:19 +0100

    Add basic vue-table

--- VueJS/wwwroot/src/apps/about.js
+++ VueJS/wwwroot/src/apps/about.js
@@ -1,1 +1,8 @@
-﻿console.log('Hello from about app.js');
+﻿import Vue from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import PaymentsTable from '../components/PaymentsTable.vue'
+
+new Vue({
+    el: '#app',
+    components: { PaymentsTable }
+});
---
+++ VueJS/wwwroot/src/components/PaymentsTable.vue
@@ -0,0 +1,14 @@
+﻿<template>
+    <vuetable ref="vuetable"
+              api-url="https://vuetable.ratiw.net/api/users"
+              :fields="['name', 'email', 'birthdate']">
+    </vuetable>
+</template>
+
+<script>
+    import Vuetable from 'vuetable-2'
+
+    export default {
+        components: { Vuetable }
+    }
+</script>
